181a8176121a2cd1d0905ee6c881e71ad9bfe43e
Dhruvsinghdk <re621006@gmail.com>
2024-08-24 16:55:11 +0530

    new octal

---
+++ Typecst.java
@@ -0,0 +1,9 @@
+public class Typecst {
+    public static void main(String[] args) {
+        int x = 056;
+        int a = 0256;
+        int b = 0285; // it gives error because it come only range of 0 to 7 not exeed
+        System.out.println(a);
+        System.out.println(x);
+    }
+}
